570bbd4191981bd9f177bd41b1a16efe37cdef84
tetsuya_ogawa <tetsuyaogawa@tetsuyaogawa-no-MacBook-Pro.local>
2016-03-27 13:27:58 +0900

    プログラム作成の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  protect_from_forgery except: :destroy
-  protect_from_forgery except: :sort
+  protect_from_forgery except: [:sort,:destroy]
+
   include SessionsHelper
   before_action :authenticate_user!
 
