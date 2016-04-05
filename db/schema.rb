b93d333b09473e9e7303f58933789820355ad6fd
tetsuya_ogawa <tetsuyaogawa@tetsuyaogawa-no-MacBook-Pro.local>
2016-04-05 13:24:48 +0900

    production設定

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403154045) do
+ActiveRecord::Schema.define(version: 20160404103612) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 20160403154045) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
