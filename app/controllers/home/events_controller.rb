983900f2503ca3e395fa695d7474c52f2316d68a
tetsuya_ogawa <tetsuyaogawa@tetsuyaogawa-no-MacBook-Pro.local>
2016-04-23 11:12:13 +0900

    一般ページのエントリー一覧ページの作成

--- app/controllers/home/events_controller.rb
+++ app/controllers/home/events_controller.rb
@@ -1,6 +1,14 @@
 class Home::EventsController < ApplicationController
 
+  layout 'public_page'
+
   def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    @groups = Group.where(event_id: params[:id])
   end
 
   def new
